d7e10bd779fe6ad552cf8e0bebcdc74d140a0231
PCalurie <peterkaruri1999@gmail.com>
2023-07-25 12:35:29 -0700

    cocktail algo

--- sort.h
+++ sort.h
@@ -25,5 +25,9 @@ void swap(int *a, int *b);
 int partition(int *array, int low, int high, size_t size);
 void quick_sort_rec(int *array, int low, int high, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
 
 #endif
