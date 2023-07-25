1f1930643d35edae36c669baff0021ff901121df
PCalurie <peterkaruri1999@gmail.com>
2023-07-25 12:29:21 -0700

    shell sort

--- sort.h
+++ sort.h
@@ -24,5 +24,6 @@ void quick_sort(int *array, size_t size);
 void swap(int *a, int *b);
 int partition(int *array, int low, int high, size_t size);
 void quick_sort_rec(int *array, int low, int high, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
